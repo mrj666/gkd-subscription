b81977a5669e913a46eb95dc85628352b14f2b6e
mrj666 <bhmrj@qq.com>
2026-04-03 17:38:46 +0800

    增加自定义的app以及完成build操作。自定义的app源文件在OWNAPP.md

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,12 +4,12 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
-  name: 'Subscription',
-  version: 0,
-  author: 'author',
+  id: 101325,
+  name: 'My GKD Subscription',
+  version: 1,
+  author: 'mrj666',
   checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/gkd-kit/subscription-template',
+  supportUri: 'https://github.com/mrj666/gkd-subscription',
   categories,
   globalGroups,
   apps: await batchImportApps(`${import.meta.dirname}/apps`),
